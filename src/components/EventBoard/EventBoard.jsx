fbf96cdbc75a0dd00352f357cf8e95cc6f10bd31
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2023-01-09 08:14:49 +0200

    save changes

--- src/components/EventBoard/EventBoard.jsx
+++ src/components/EventBoard/EventBoard.jsx
@@ -2,12 +2,20 @@ import PropTypes from "prop-types"
 import { Event } from "components/Event/Event";
 import { Container } from "components/EventBoard/EventBoard.styled";
 
+
+
+
+
 export const EventBoard=({events})=>{
     return <Container>
         {events.map(({name,location,speaker,type,time})=><Event key={name}  name={name} location={location} speaker={speaker} type={type} start={time.start} end={time.end}>
-          
-        </Event>)}</Container>
+            </Event>)}
+    </Container>
 };
+
+
+
+
 
 
 EventBoard.propTypes={
